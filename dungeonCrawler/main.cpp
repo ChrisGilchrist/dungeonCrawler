5f6ae50256789cf4bfeb9ee5fda4dab8060ce1f8
Gilly <gilly@sgnet.co.uk>
2019-10-04 14:43:47 +0100

    Added a new map called map2. Made it dynamic in finding the start location on start up.

--- dungeonCrawler/main.cpp
+++ dungeonCrawler/main.cpp
@@ -6,9 +6,27 @@
 
 using namespace std;
 
+// Stores the 2d Matrix of the map
+/*
+		###############################
+		#.............................#
+	y	#.G...........................#
+		#.............................#
+	a	#.............########........#
+	x	#.............#..E.#M.........#
+	i	#.............#...............#
+	s	#.S...........########........#
+		#................G#G..........#
+		###############################
+
+		<----------- x axis ----------->
+*/
 vector<vector<char>> map;
+
+// Global count of gold picked up by the user
 int goldCount = 0;
 
+// Timestamps
 auto startTime = chrono::system_clock::now();
 auto endTime = chrono::system_clock::now();
 
@@ -19,7 +37,9 @@ const char MonsterSymbol = 'M';
 const char GoldSymbol = 'G';
 const char EmptySymbol = '.';
 const char WallSymbol = '#';
+const char SpawnSymbol = 'S';
 
+// Keys that will be clicked by the user
 enum Keys
 {
 	Right = 'd',
@@ -29,6 +49,7 @@ enum Keys
 	Quit = 'q'
 };
 
+// Various states the game can be in
 enum GameState
 {
 	HitMonster,
@@ -41,8 +62,8 @@ void createMap() {
 	string STRING;
 	ifstream infile;
 
-	//  Opens file
-	infile.open("map.txt");
+	// infile.open("map.txt"); // Opens the first map
+	infile.open("map2.txt"); // Opens the second map
 
 	// If the file opens correctly then start looping
 	if (infile.is_open()) {
@@ -71,6 +92,7 @@ void createMap() {
 	}
 }
 
+// Reprint the map each time the user makes a move
 void printMap() {
 	for (int y = 0; y < map.size(); ++y) {
 
@@ -81,12 +103,44 @@ void printMap() {
 	}
 }
 
+// Move the monster in a specific direction
 void moveMonster(
 	int& currentPosX, int& currentPosY,
 	int desiredPosX, int desiredPosY) {
-	
 
+	// First Check the user is not out of the map
+	if (desiredPosX < 0) { desiredPosX = 0; }
+	if (desiredPosY < 0) { desiredPosY = 0; }
 
+	// Check that their next move does not make them go out the map
+	if (desiredPosX == 0 || desiredPosX == map.size() - 1) { desiredPosX = currentPosX; };
+	if (desiredPosY == 0 || desiredPosY == map[0].size() - 1) { desiredPosY = currentPosY; };
+
+	if (map[desiredPosX][desiredPosY] == WallSymbol) {
+		// Stops the Wall from being consumed by the monster
+		desiredPosX = currentPosX;
+		desiredPosY = currentPosY;
+	} 
+	else if (map[desiredPosX][desiredPosY] == ExitSymbol) {
+		// Stops the Exit from being consumed by the monster
+		desiredPosX = currentPosX;
+		desiredPosY = currentPosY;
+	}
+	else if (map[desiredPosX][desiredPosY] == PlayerSymbol) {
+		// The monster caught the player when it moved
+		cout << "GAME OVER - A monster got you first!" << '\n';
+		cout << "Gold Count: " << goldCount << '\n';
+		exit(0);
+	} 
+
+	// Clear current position
+	map[currentPosX][currentPosY] = EmptySymbol;
+
+	// Place the player symbol
+	map[desiredPosX][desiredPosY] = MonsterSymbol;
+
+	currentPosX = desiredPosX;
+	currentPosY = desiredPosY;
 }
 
 GameState movePlayer(
@@ -117,7 +171,6 @@ GameState movePlayer(
 		cout << "Gold Count: " << goldCount << '\n';
 		cout << "Elapsed Time: " << elapsed_seconds.count() << '\n';
 		
-
 		newGameState = FoundExit;
 	}
 	else if (map[desiredPosX][desiredPosY] == GoldSymbol) {
@@ -147,8 +200,11 @@ GameState movePlayer(
 }
 
 int main() {
-	int playerPosX = 7;
-	int playerPosY = 3;
+	int playerPosX = 0;
+	int playerPosY = 0;
+
+	int monsterPosX = 0;
+	int monsterPosY = 0;
 
 	GameState gameState = StillLooking;
 
@@ -163,13 +219,24 @@ int main() {
 
 		for (int y = 0; y < map[0].size(); ++y) {
 
-			if (map[x][y] == PlayerSymbol) {
+			if (map[x][y] == SpawnSymbol) {
 				playerPosX = x;
 				playerPosY = y;
-				printMap();
 			}
 		}
 	} 
+
+	/* In the vector, find where the monsters current location is by looking for 'M' */
+	for (int x = 0; x < map.size(); ++x) {
+
+		for (int y = 0; y < map[0].size(); ++y) {
+
+			if (map[x][y] == MonsterSymbol) {
+				monsterPosX = x;
+				monsterPosY = y;
+			}
+		}
+	}
 
 	// While we are still looking, listen for key presses from user
 	while (gameState == StillLooking) {
@@ -204,7 +271,21 @@ int main() {
 		}
 		
 		// Generate a random direcition for the monster to move in
-		// moveMonster();
+		int randomNo = rand() % 4 + 1;
+		switch (randomNo) {
+		case 1:
+			moveMonster(monsterPosX, monsterPosY, monsterPosX, monsterPosY + 1);
+			break;
+		case 2:
+			moveMonster(monsterPosX, monsterPosY, monsterPosX, monsterPosY - 1);
+			break;
+		case 3:
+			moveMonster(monsterPosX, monsterPosY, monsterPosX - 1, monsterPosY);
+			break;
+		case 4:
+			moveMonster(monsterPosX, monsterPosY, monsterPosX + 1, monsterPosY);
+			break;
+		}
 
 		printMap();
 
